a3bdf7d31c0aa0d409c7f24f73f6080ee0dd8f4b
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-12 08:07:40 +0100

    New example, but for WiFi connection

---
+++ sketch_feb12a/sketch_feb12a.ino
@@ -0,0 +1,18 @@
+#include <WiFi.h>
+
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
+
+const char* host = "data.sparkfun.com";
+const char* streamId   = "....................";
+const char* privateKey = "....................";
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
