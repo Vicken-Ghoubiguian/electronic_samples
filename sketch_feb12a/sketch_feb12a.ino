a16b1c694887f6bb380facd3937f8b57cc2ad596
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-12 08:53:47 +0100

    Other things very important

--- sketch_feb12a/sketch_feb12a.ino
+++ sketch_feb12a/sketch_feb12a.ino
@@ -1,15 +1,72 @@
+#include <heltec.h>
 #include <WiFi.h>
 
-const char* ssid     = "your-ssid";
-const char* password = "your-password";
+const char* ssid     = "IMERIR";
+const char* password = "eric@1992";
 
-const char* host = "data.sparkfun.com";
-const char* streamId   = "....................";
-const char* privateKey = "....................";
+#define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
 
+  Heltec.display->flipScreenVertically();
+  Heltec.display->setFont(ArialMT_Plain_10);
+
+  Heltec.display->clear();
+
+  WiFi.begin(ssid, password);
+
+  if(WiFi.status() == WL_CONNECTED) {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "Connection to Wifi successful...");
+    
+  } else if(WiFi.status() == WL_NO_SHIELD) {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "No shield available for Wifi connection...");
+    
+  } else if(WiFi.status() == WL_CONNECT_FAILED) {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "Connection to Wifi failed...");
+
+  } else if(WiFi.status() == WL_NO_SSID_AVAIL) {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "No Wifi network available...");
+    
+  } else if(WiFi.status() == WL_SCAN_COMPLETED) {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "Scan of all Wifi networks completed...");
+    
+  } else if(WiFi.status() == WL_CONNECTION_LOST) {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "Wifi connection lost...");
+    
+  } else if(WiFi.status() == WL_DISCONNECTED) {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "Wifi connection disconnected...");
+    
+  } else {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "Sending a LoRa packet...");
+  }
+
+  Heltec.display->display();
 }
 
 void loop() {
