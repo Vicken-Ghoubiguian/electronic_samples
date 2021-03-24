0feade984be14b86bd457e18c372fb1355961866
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-03-24 15:31:25 +0100

    Some things updates or added

--- raspberry_pi_pico/Components_testing_tutorials/sample_2.py
+++ raspberry_pi_pico/Components_testing_tutorials/sample_2.py
@@ -3,7 +3,7 @@ from machine import Pin
 
 # definition of 2 leds at respectively pin 7 and 13...
 led_1 = Pin(7, Pin.OUT)
-led_2 = Pin(13, Pin.OUT)
+led_2 = Pin(17, Pin.OUT)
 
 # definition of the luminosity value of each led
 led_1.value(0.5)
