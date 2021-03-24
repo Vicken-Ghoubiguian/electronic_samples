45bf46be48a39d15b873ced28a4ced4ec859afb0
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-03-24 12:02:22 +0100

    Writing comments

--- raspberry_pi_pico/Components_testing_tutorials/sample_1.py
+++ raspberry_pi_pico/Components_testing_tutorials/sample_1.py
@@ -1,10 +1,10 @@
 # import 'Pin' (to manipulate pins) from 'machine' module...
 from machine import Pin
 
-#
+# definition of 2 leds at respectively pin 7 and 13...
 led_1 = Pin(7, Pin.OUT)
 led_2 = Pin(13, Pin.OUT)
 
-#
+# definition of the luminosity value of each led
 led_1.value(0.5)
 led_2.value(0.5)
