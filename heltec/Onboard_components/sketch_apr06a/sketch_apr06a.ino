97cbd8f8a27b30fa9df43bb43ae11e5b57ae8269
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-04-06 12:00:50 +0200

    Other modifications

--- heltec/Onboard_components/sketch_apr06a/sketch_apr06a.ino
+++ heltec/Onboard_components/sketch_apr06a/sketch_apr06a.ino
@@ -37,4 +37,38 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  //
+  if(WiFi.status() == WL_CONNECTED){
+
+    //
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "Connection to Wifi successful...");
+
+    //
+    WiFiClient httpClient = server.available();
+    
+    //
+    if(httpClient){
+      
+      Heltec.display->drawString(10, 0, "New Client...MIRACLE...");
+      Heltec.display->display();
+      
+    } else {
+  
+      Heltec.display->drawString(10, 0, "No clients...Snif...Snif...");
+      Heltec.display->display();
+    }
+  
+    //
+    Heltec.display->clear();
+    
+  } else {
+
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 10, "Sorry, but not connected...");
+  
+    Heltec.display->display();
+  }
 }
