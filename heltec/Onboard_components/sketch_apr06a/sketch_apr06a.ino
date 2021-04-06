6279b4d979049edf2a630ba7af7d20d57fbd4229
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-04-06 09:52:24 +0200

    Complete the sample

--- heltec/Onboard_components/sketch_apr06a/sketch_apr06a.ino
+++ heltec/Onboard_components/sketch_apr06a/sketch_apr06a.ino
@@ -1,8 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * 
+ * 
+ */
 
+#include <heltec.h>
+#include <WiFi.h>
+
+const char* ssid     = "Livebox-681C";
+const char* password = "43E13C340F4222E66C12499CE7";
+
+#define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
+
+WiFiServer server(80);
+
+//
+void setup() {
+
+  // put your setup code here, to run once:
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+
+  //
+  Heltec.display->flipScreenVertically();
+  Heltec.display->setFont(ArialMT_Plain_10);
+
+  //
+  Heltec.display->clear();
+
+  //
+  WiFi.begin(ssid, password);
+
+  //
+  delay(10000);
 }
 
+//
 void loop() {
   // put your main code here, to run repeatedly:
 
